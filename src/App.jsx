0881c61f2b236efb827de8200544be9fc29cadfb
Karol Gralewski <gralewski.karol@gmail.com>
2023-06-26 16:11:52 +0200

    Initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+
+function App() {
+  return <h1 className="font-bold">Hello world</h1>;
+}
+
+export default App;
