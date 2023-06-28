693982443c11771b5bc9dceedb1f9caf21870a0c
Karol Gralewski <gralewski.karol@gmail.com>
2023-06-28 20:19:37 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import React from 'react';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [React()],
+  plugins: [react()],
   base: '/lubelskie-it/',
 });
