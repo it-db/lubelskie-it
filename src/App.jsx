c119017094c89bf64854cd00952f7b3f614a5afd
Karol Gralewski <gralewski.karol@gmail.com>
2023-06-28 21:11:55 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './components/pages/Home';
-import {DataContextProvider} from './context/DataContextProvider';
+import { DataContextProvider } from './context/DataContextProvider';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={
-        <DataContextProvider>
-          <Home />
-        </DataContextProvider>
-      } />
-    </Routes>
-  );
+  return <div>test</div>;
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-  <BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
