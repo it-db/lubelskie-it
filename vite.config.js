88edaaa72ad264d214f859004fbf9c4fd226e710
Johnny <jangryta9@gmail.com>
2023-06-28 17:42:18 +0200

    websiteTest

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/lubelskie-it/'
+  base: "/lubelskie-it/"
 });
