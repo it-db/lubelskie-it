92052daa118be5238bc7b2217d17d0f33309764a
Karol Gralewski <gralewski.karol@gmail.com>
2023-06-28 15:32:39 +0200

    Final touches

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -95,7 +95,6 @@ export const Home = () => {
     <div className="h-auto min-h-screen bg-neutral-900 py-5">
       <div className="flex w-full flex-col items-center justify-center pb-10">
         <Link
-          navigateTo="/"
           onClick={() => {
             window.location.reload();
           }}
@@ -160,7 +159,7 @@ export const Home = () => {
       </div>
 
       <div className="fixed bottom-0 mt-10 flex w-full flex-col items-center justify-center border-t-2 border-neutral-800/80  bg-neutral-900 py-2">
-        <div className="mb-2 text-sm font-semibold text-neutral-200">Podoba Ci się inicjatywa? Wesprzyj nas na GitHubie!</div>
+        <div className="mb-2 text-sm font-semibold text-neutral-200">Wesprzyj nas na GitHubie!</div>
         <div>
           <GitHubButton href="https://github.com/it-db/lubelskie-it" className="" data-color-scheme="no-preference: dark-dimmed; light: dark-dimmed; dark: dark-dimmed;" data-size="medium" data-show-count="true" aria-label="Star it-db/lubelskie-it on GitHub">
             Star
