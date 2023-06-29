432b43d64235fb4ca2390c67d2b31070c3e48e5a
Johnny <jangryta9@gmail.com>
2023-06-29 10:10:59 +0200

    merged

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: '/lubelskie-it/',
   base: '/lubelskie-it/'
 
 });
